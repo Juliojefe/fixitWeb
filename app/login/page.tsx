76945f0fc7a192135d0e0d40c5da4571388a8681
Julio Cesar Fernandez <j.c.jf285@gmail.com>
2025-11-21 10:42:55 -0800

    working login non-google user page with basic styles

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,41 +12,77 @@ export default function Home() {
   const [password, setPassword] = React.useState("");
   const [errorMessage, setErrorMessage] = React.useState("");
 
-  async function doNothing() {
-    return;
-  }
+  console.log("API:", process.env.NEXT_PUBLIC_API_URL);
 
-  async function handleSubmit() {
-    return;
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setErrorMessage("");
+    try {
+      const responseData = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`, {
+        email: email,
+        password: password
+      });
+      const authData = responseData.data;
+      if (authData.accessToken && authData.refreshToken) {
+        /**
+         * persist repsonse data for sessions
+         */
+        // router.push("/home");
+        alert("working");
+      } else {
+        setErrorMessage("Unexpected response from server.");
+      }
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response) {
+        setErrorMessage(err.response.data.message || "Login Failed");
+      } else {
+        setErrorMessage("Network error");
+      }
+    }
   }
 
   async function handleContinueWithGoogle() {
-
+    //  Do nothing for now
+    return;
   }
 
   return (
     <div className={styles.container}>
-      <form onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        <label>Email
+      <form className={styles.loginForm} onSubmit={handleSubmit}>
+        <h2 className={styles.formHeader}>Login</h2>
+        <label className={styles.formLable}>Email
           <input
             type="email"
             value={email}
             required
-            onChange={doNothing}
+            className={styles.formInput}
+            onChange={e => setEmail(e.target.value)}
           />
         </label>
-        <label>Password
+        <label className={styles.formLable}>Password
           <input
             type="password"
             value={password}
             required
-            onChange={doNothing}
+            className={styles.formInput}
+            onChange={e => setPassword(e.target.value)}
           />
         </label>
-        <button type="submit"> Login </button>
-        <button type="button" onClick={async() => router.push("/signUp")}> Create Account </button>
-        <button type="button" onClick={handleContinueWithGoogle}> Continue with Google </button>
+        <button className={styles.loginBtn} type="submit"> Login </button>
+        <button className={styles.signUpBtn} type="button" onClick={async () => router.push("/signUp")}> Create Account </button>
+        <button
+          className={styles.googleBtn}
+          type="button"
+          onClick={handleContinueWithGoogle}
+        >
+          <img
+            className={styles.googleIcon}
+            src="/icons/googleLogo.png"
+            alt="Google logo"
+          />
+          Continue with Google
+        </button>        
         {errorMessage && <p className={styles.error}>{errorMessage}</p>}
       </form>
     </div>
