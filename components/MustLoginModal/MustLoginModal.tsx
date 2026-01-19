0947516353b28907e796653450d79b31e2a05aa5
Julio Cesar Fernandez <j.c.jf285@gmail.com>
2026-01-19 13:19:16 -0800

    Guest modal created

--- components/MustLoginModal/MustLoginModal.tsx
+++ components/MustLoginModal/MustLoginModal.tsx
@@ -2,19 +2,24 @@ import { useRouter } from "next/navigation";
 import styles from './MustLoginModal.module.css'
 
 interface MustLoginModalProps {
-  message? : string;
+  message?: string;
   onClose?: () => void;
 }
 
-export default function MustLoginModal({ message = "Log in to access this feature.", onClose } : MustLoginModalProps ) {
+export default function MustLoginModal({ message = "Log in to access this feature.", onClose }: MustLoginModalProps) {
   const router = useRouter();
   return (
-    <div className={styles.modalForm} onClick={(e) => e.stopPropagation()}>
-      <h2 className={styles.formHeader}>{message}</h2>
-      <h3 className={styles.formSubHeader}>Already have an account?</h3>
-      <button className={styles.primaryBtn} type="button" onClick={async () => router.push("/login")}> Login </button>
-      <h3 className={styles.formSubHeader}>New?</h3>
-      <button className={styles.secondaryBtn} type="button" onClick={async () => router.push("/signUp")}> Create Account </button>
+    <div
+      className={styles.backdrop}
+      onClick={onClose}
+    >
+      <div className={styles.modalForm} onClick={(e) => e.stopPropagation()}>
+        <h2 className={styles.formHeader}>{message}</h2>
+        <h3 className={styles.formSubHeader}>Already have an account?</h3>
+        <button className={styles.primaryBtn} type="button" onClick={async () => router.push("/login")}> Login </button>
+        <h3 className={styles.formSubHeader}>New?</h3>
+        <button className={styles.secondaryBtn} type="button" onClick={async () => router.push("/signUp")}> Create Account </button>
+      </div>
     </div>
   )
 }
