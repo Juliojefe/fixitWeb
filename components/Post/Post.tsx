9f823faeafafd3b8eb21f3368809c06bb233e7b8
Julio Cesar Fernandez <j.c.jf285@gmail.com>
2026-01-19 13:33:08 -0800

    guest like/save behavior handled

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -64,71 +64,77 @@ export default function Post({ postData = null }: PostProps) {
   }
 
   return (
-    <div className={styles.postContainer}>
-      {/* header section */}
-      <div
-        className={styles.headerSection}
-        onClick={
-          deletedAuthor
-            ? undefined
-            : () => handleGoToProfile(postData.authorId)
-        }
-        style={deletedAuthor ? { cursor: "default" } : undefined}
-      >
-        <img
-          className={styles.profilePic}
-          src={
+    <>
+      {showLoginModal && (
+        <MustLoginModal onClose={() => setShowLoginModal(false)} />
+      )}
+
+      <div className={styles.postContainer}>
+        {/* header section */}
+        <div
+          className={styles.headerSection}
+          onClick={
             deletedAuthor
-              ? "/images/deletedUserPfp.png"
-              : postData.createdByProfilePicUrl
+              ? undefined
+              : () => handleGoToProfile(postData.authorId)
           }
-          alt="profile picture"
-        />
-        <p className={styles.userName}>
-          {deletedAuthor ? "Deleted User" : postData.createdBy}
-        </p>
-      </div>
-      {/* Image Section */}
-      {hasImage ? (
-        <div className={styles.imageSection}>
-          {postData.imageUrls?.[0] && (
-            <img
-              className={styles.postImage}
-              src={postData.imageUrls[currImageIndex]}
-              alt="post image"
-            />
-          )}
-          {postData.imageUrls.length > 1 && currImageIndex > 0 && (
-            <button className={styles.prevImageButton} onClick={handleShowPrevImage}>
-              <FaChevronLeft />
-            </button>
-          )}
-          {postData.imageUrls.length > 1 && currImageIndex < postData.imageUrls.length - 1 && (
-            <button className={styles.nextImageButton} onClick={handleShowNextImage}>
-              <FaChevronRight />
-            </button>
-          )}
+          style={deletedAuthor ? { cursor: "default" } : undefined}
+        >
+          <img
+            className={styles.profilePic}
+            src={
+              deletedAuthor
+                ? "/images/deletedUserPfp.png"
+                : postData.createdByProfilePicUrl
+            }
+            alt="profile picture"
+          />
+          <p className={styles.userName}>
+            {deletedAuthor ? "Deleted User" : postData.createdBy}
+          </p>
         </div>
-      ) : null}
-      {/* description section */}
-      <p className={styles.postDescription}>{postData.description || ""}</p>
-      {/* like count */}
-      <p className={styles.likeCount}>{postData.likeCount || 0} likes</p>
-      {/* like comment save icons */}
-      <div className={styles.actionIcons}>
-        <FaRegHeart
-          className={styles.icon}
-          onClick={() => handleLike()}
-        />
-        <FaRegComment
-          className={styles.icon}
-          onClick={() => handleComments()}
-        />
-        <FaRegBookmark
-          className={styles.icon}
-          onClick={() => handleSave()}
-        />
+        {/* Image Section */}
+        {hasImage ? (
+          <div className={styles.imageSection}>
+            {postData.imageUrls?.[0] && (
+              <img
+                className={styles.postImage}
+                src={postData.imageUrls[currImageIndex]}
+                alt="post image"
+              />
+            )}
+            {postData.imageUrls.length > 1 && currImageIndex > 0 && (
+              <button className={styles.prevImageButton} onClick={handleShowPrevImage}>
+                <FaChevronLeft />
+              </button>
+            )}
+            {postData.imageUrls.length > 1 && currImageIndex < postData.imageUrls.length - 1 && (
+              <button className={styles.nextImageButton} onClick={handleShowNextImage}>
+                <FaChevronRight />
+              </button>
+            )}
+          </div>
+        ) : null}
+        {/* description section */}
+        <p className={styles.postDescription}>{postData.description || ""}</p>
+        {/* like count */}
+        <p className={styles.likeCount}>{postData.likeCount || 0} likes</p>
+        {/* like comment save icons */}
+        <div className={styles.actionIcons}>
+          <FaRegHeart
+            className={styles.icon}
+            onClick={() => handleLike()}
+          />
+          <FaRegComment
+            className={styles.icon}
+            onClick={() => handleComments()}
+          />
+          <FaRegBookmark
+            className={styles.icon}
+            onClick={() => handleSave()}
+          />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
