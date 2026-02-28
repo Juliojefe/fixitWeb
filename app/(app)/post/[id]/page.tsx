8a2aabbb74ba18bd06153b602babbe08ca2dfc94
Juliojefe <j.c.jf285@gmail.com>
2026-02-28 09:18:56 -0800

    Fetching post by id for guest and auth users

--- app/(app)/post/[id]/page.tsx
+++ app/(app)/post/[id]/page.tsx
@@ -1,12 +1,40 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import { DisplayPostType } from "@/types/displayPost";
+import Post from "@/components/Post/Post";
+import { useUser } from '@/app/providers/UserProvider';
+import axios from "axios";
+import { useState } from "react";
 
 export default function PostPage() {
-  const params = useParams<{ id: string }>();
-  const idNumber = Number(params.id);
+    const params = useParams<{ id: string }>();
+    const id = Number(params.id);
+    const { user } = useUser();
+    const [postData, setPostData] = useState<DisplayPostType | null>(null);
 
-  console.log("Post ID as number:", idNumber);
+    async function fetchPost() {
+        let endpoint = `${process.env.NEXT_PUBLIC_API_URL}/api/post/${id}`
+        let config = {}
+        try {
+            if (user?.accessToken) {
+                config = {
+                    headers: {
+                        Authorization: `Bearer ${user.accessToken}`,
+                    },
+                };
+            }
+            const res = await axios.get(endpoint, config);
+            setPostData(res.data);
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
-  return <h1>Post {idNumber}</h1>;
+    fetchPost();
+
+    return (
+        <Post postData={postData}></Post>
+    );
+    
 }
